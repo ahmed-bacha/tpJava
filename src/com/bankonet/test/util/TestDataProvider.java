dfde74bef3a7499731a6254281b73d83bc922c5d
Ahmed Bacha <ahmedbachaabdelkrim@gmail.com>
2014-10-10 13:54:34 +0200

    Fix #1

--- src/com/bankonet/test/util/TestDataProvider.java
+++ src/com/bankonet/test/util/TestDataProvider.java
@@ -10,13 +10,13 @@ public class TestDataProvider {
 	public static SoldeConsultable[] buildSoldeConsultableData(){
 		SoldeConsultable[] result = new SoldeConsultable[6];
 		
-		result[0] = new CompteEpargne("CE0","CE0", 1000.0, 1.5);
+		result[0] = new CompteEpargne("CE0","CE0", 1000.0, 1.5, 5000.0);
 		result[1] = new CompteCourant("CC1","CC1", 2000.0, 200.0);
 		
-		result[2] = new CompteEpargne("CE1","CE1", 3000.0, 1.5);
+		result[2] = new CompteEpargne("CE1","CE1", 3000.0, 1.5,5000.0);
 		result[3] = new CompteCourant("CC2","CC2", 4000.0, 200.0);
 		
-		result[4] = new CompteEpargne("CE2","CE2", 5000.0, 1.5);
+		result[4] = new CompteEpargne("CE2","CE2", 5000.0, 1.5,5000.0);
 		result[5] = new CompteCourant("CC3","CC3", 6000.0, 200.0);
 		
 		return result;
